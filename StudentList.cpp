5a6e1dc2cb77a17da55ebd8f35f1bd2f09d078ff
U-BSD\301968 <301968@SHS-10D8A7CD2.beaverton.k12.or.us>
2016-10-05 14:22:43 -0700

    finished adding students to vector

--- StudentList.cpp
+++ StudentList.cpp
@@ -1,20 +1,51 @@
 #include <iostream>
 #include <vector>
+#include <string.h>
 
 using namespace std;
 
 struct Student {
-  char name[10];
+  char nameFirst[81];
+  char nameLast[81];
   int id;
   float gpa;
 };
 
+
 int main()
 {
-  char input[21];
-  cout << "Awaiting input: ";
-  cin.getline(input, 21);
-  cout << input << endl;
-
+  vector<Student> vec;
+  Student s;
+  
+  bool running = true;
+  char input[81];
+  char add[4] = {'A','D','D','\0'};
+  char print[6] = {'P','R','I','N','T','\0'};
+  char del[8] = {'D','E','L','E','T','E','\0'};
+  
+  while(running){
+    fill(input, input + 81, ' ');
+    cout << "Awaiting input: ";
+    cin.getline(input, 81);
+    if (strcmp(input, add) == 0){
+      cout << "ADDING STUDENT" << endl;
+      cout << "Enter first name: ";
+      cin.getline(s.nameFirst, 81);
+      cout << "Enter last name: ";
+      cin.getline(s.nameLast, 81);
+      cout << "Enter student ID: ";
+      cin >> s.id;
+      cout << "Enter student GPA: ";
+      cin >> s.gpa;
+      cin.ignore();
+      vec.push_back(s);
+    }
+    else if(strcmp(input, print) == 0){
+      cout << "print";
+    }
+    else if(strcmp(input, del) == 0){
+	cout << "delete" << endl;
+    }
+  }
   return 0;
 }
