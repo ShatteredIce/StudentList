3064c1db6579b2050955b714b05895cfd757e1a9
U-BSD\301968 <301968@SHS-10D8A7CD2.beaverton.k12.or.us>
2016-10-11 14:21:00 -0700

    finished program

--- StudentList.cpp
+++ StudentList.cpp
@@ -24,13 +24,19 @@ int main()
   char add[4] = {'A','D','D','\0'};
   char print[6] = {'P','R','I','N','T','\0'};
   char del[8] = {'D','E','L','E','T','E','\0'};
+  char quit[5] = {'Q','U','I','T','\0'};
+  int idDelete = 0;
   
   while(running){
     fill(input, input + 81, ' ');
     cout << "Awaiting input: ";
     cin.getline(input, 81);
-    if (strcmp(input, add) == 0){
-      cout << "ADDING STUDENT" << endl;
+    if(strcmp(input, quit) == 0){
+      cout << "Program Terminated." << endl;
+      running = false;
+    }
+    else if (strcmp(input, add) == 0){
+      cout << endl << "-----ADDING STUDENT-----" << endl;
       cout << "Enter first name: ";
       cin.getline(s.nameFirst, 81);
       cout << "Enter last name: ";
@@ -45,17 +51,25 @@ int main()
     }
     else if(strcmp(input, print) == 0){
       if(vec.size() == 0){
-	cout << "NO STUDENTS" << endl;
+	cout << endl << "-----NO STUDENTS-----" << endl << endl;
       }
       else{
-	cout << "STUDENT INFORMATION" << endl;
+	cout << endl << "-----STUDENT INFORMATION-----" << endl;
 	for(int a = 0; a < vec.size(); a++){
 	  cout << vec[a].nameFirst << " " << vec[a].nameLast << ", " << vec[a].id << ", " << setprecision(2) << fixed << vec[a].gpa << endl;
 	}
       }
     }
     else if(strcmp(input, del) == 0){
-	cout << "delete" << endl;
+      cout << endl << "-----DELETING STUDENT-----" << endl;
+      cout << "Enter student ID: ";
+      cin >> idDelete;
+      cin.ignore(80, '\n');
+      for(int a = 0; a < vec.size(); a++){
+	if(idDelete == vec[a].id){
+	  vec.erase(vec.begin()+a);
+	}
+      }
     }
   }
   return 0;
