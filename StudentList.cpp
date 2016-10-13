60246444ce02555c3fed5bd7acf4cdb5bebd85af
U-BSD\301968 <301968@SHS-10D8A7CD2.beaverton.k12.or.us>
2016-10-13 13:21:22 -0700

    added comments

--- StudentList.cpp
+++ StudentList.cpp
@@ -1,3 +1,6 @@
+//C++ Project 4 - Nathan Purwosumarto - 10/13/16
+//StudentList - Allows the user to store students information
+
 #include <iostream>
 #include <vector>
 #include <string.h>
@@ -6,6 +9,7 @@
 
 using namespace std;
 
+//Student variables
 struct Student {
   char nameFirst[81];
   char nameLast[81];
@@ -16,9 +20,11 @@ struct Student {
 
 int main()
 {
+  //create new vector
   vector<Student> vec;
   Student s;
-  
+
+  //variable setup
   bool running = true;
   char input[81];
   char add[4] = {'A','D','D','\0'};
@@ -26,15 +32,18 @@ int main()
   char del[8] = {'D','E','L','E','T','E','\0'};
   char quit[5] = {'Q','U','I','T','\0'};
   int idDelete = 0;
-  
+
+  //get user input while the program is running
   while(running){
     fill(input, input + 81, ' ');
     cout << "Awaiting input: ";
     cin.getline(input, 81);
+    //if input = QUIT, then end the program
     if(strcmp(input, quit) == 0){
       cout << "Program Terminated." << endl;
       running = false;
     }
+    //if input = ADD, prompt user for student information to add
     else if (strcmp(input, add) == 0){
       cout << endl << "-----ADDING STUDENT-----" << endl;
       cout << "Enter first name: ";
@@ -49,6 +58,7 @@ int main()
       cin.ignore(80, '\n');
       vec.push_back(s);
     }
+    //if input = PRINT, then print all students stored, tell user if there are no students
     else if(strcmp(input, print) == 0){
       if(vec.size() == 0){
 	cout << endl << "-----NO STUDENTS-----" << endl << endl;
@@ -60,6 +70,7 @@ int main()
 	}
       }
     }
+    //if input = DELETE, prompt user for student ID to be deleted, then delete a student with that ID if one exists
     else if(strcmp(input, del) == 0){
       cout << endl << "-----DELETING STUDENT-----" << endl;
       cout << "Enter student ID: ";
